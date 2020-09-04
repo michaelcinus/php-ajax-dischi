2010f0dde5f0eda94ead866a0e1eae3c29b56b69
Michael Cinus <michaelcinus@gmail.com>
2020-09-04 14:25:15 +0200

    versione Solo con l'utilizzo di PHP

---
+++ index.php
@@ -0,0 +1,39 @@
+<?php
+
+include __DIR__ . '/partials/vars.php';
+
+?>
+
+<body style='background-color:#1E2D3B;
+             width:100%;
+             margin:auto;
+             display:flex;
+             flex-direction:row;
+             flex-wrap:wrap;'>
+
+    <?php 
+
+    foreach ($data['response'] as $album) {
+
+        ?>
+    <div style='width: 16%;
+                margin: 3% 2%;
+                height: 50%;
+                background: rgb(46, 58, 70);
+                text-align: center;
+                color: white;'>
+       <?php
+
+        echo  ' <img style="70%:auto;height:40%;margin-top:5%;" src="' . $album['poster'] . '" alt=""> ';   
+        echo '<h4>' . $album['title'] . '</h4>' . '<br>';
+        echo $album['author'] . '<br>';
+        echo $album['genre'] .'<br>';
+        echo $album['year'] . '<br>';
+
+        ?>
+    
+    </div>
+
+    <?php } ?>
+
+</body>
